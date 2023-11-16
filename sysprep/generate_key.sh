4b94c751135638fbf2cb89713e889e153a93ea93
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2023-11-15 20:51:43 -0500

    Remove the generate_key.sh script

--- sysprep/generate_key.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-test -e private_key || ssh-keygen -t ed25519 -f private_key -N ""
-chmod 0600 private_key
-vault write -field=signed_key ssh/sign/packer-role public_key=@private_key.pub > private_key-cert.pub
-ssh-keygen -Lf private_key-cert.pub
