b40b3412a0d17e1b86cdd62533479b0251e82458
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-09-30 00:45:10 +0000

    Add vhost-2 workspace

---
+++ 02-vm-provisioner/vhost-2/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.8.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "tenzin-io"
+    key            = "terraform/vhost-2.state"
+    dynamodb_table = "tenzin-io"
+    region         = "us-east-1"
+  }
+}
+
+provider "libvirt" {
+  uri = "qemu+ssh://root@vhost-2.lan/system?keyfile=ansible.key&sshauth=privkey&no_verify=1"
+}
+
+// host
+module "hypervisor" {
+  source          = "git::https://github.com/tenzin-io/terraform-modules.git//libvirt/hypervisor?ref=main"
+  hypervisor_ip   = "192.168.200.252"
+  vm_network_cidr = "10.255.2.0/24"
+  vm_domain_name  = "vm.vhost-2"
+}
+
+// virtual machines on vhost_2
+module "kube_1" {
+  count           = 1
+  source          = "git::https://github.com/tenzin-io/terraform-modules.git//libvirt/virtual-machine?ref=main"
+  name            = "kube-1"
+  datastore_name  = module.hypervisor.datastore_name
+  network_id      = module.hypervisor.network_id
+  base_volume_id  = module.hypervisor.base_volume_id
+  cpu_count       = 4
+  memory_size_mib = 12 * 1024  // gib
+  disk_size_mib   = 200 * 1024 // gib
+  addresses       = ["10.255.2.11"]
+  gpu_pci_bus     = "07"
+}
