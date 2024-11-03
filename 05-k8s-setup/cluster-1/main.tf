39281acfc8e140b65f919b6a8480a4ce4c0638bb
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-11-03 16:21:45 +0000

    Enable other modules

--- 05-k8s-setup/cluster-1/main.tf
+++ 05-k8s-setup/cluster-1/main.tf
@@ -58,52 +58,52 @@ module "nginx_ingress" {
   depends_on               = [module.cert_manager]
 }
 
-# module "prometheus" {
-#   depends_on          = [module.cert_manager, module.local_path_provisioner]
-#   enable_ingress      = true
-#   enable_basic_auth   = true
-#   basic_auth_password = data.vault_generic_secret.basic_auth.data["htpasswd"]
-#   prometheus_fqdn     = "prometheus.vm.vhost-1"
-#   source              = "git::https://github.com/tenzin-io/terraform-modules.git//kubernetes/prometheus?ref=main"
-# }
+module "prometheus" {
+  depends_on          = [module.cert_manager, module.local_path_provisioner]
+  enable_ingress      = true
+  enable_basic_auth   = true
+  basic_auth_password = data.vault_generic_secret.basic_auth.data["htpasswd"]
+  prometheus_fqdn     = "prometheus.vm.vhost-1"
+  source              = "git::https://github.com/tenzin-io/terraform-modules.git//kubernetes/prometheus?ref=main"
+}
 
-# module "actions_runner" {
-#   source     = "git::https://github.com/tenzin-io/terraform-modules.git//kubernetes/actions-runner?ref=main"
-#   depends_on = [module.calico]
-# }
-# 
-# module "actions_runner_org" {
-#   depends_on                 = [module.actions_runner]
-#   runner_set_name            = "k8s-1-runners"
-#   source                     = "git::https://github.com/tenzin-io/terraform-modules.git//kubernetes/actions-runner-set?ref=main"
-#   runner_image               = "ghcr.io/tenzin-io/actions-runner:0.0.3"
-#   github_config_urls         = ["https://github.com/tenzin-io"]
-#   github_app_id              = data.vault_generic_secret.github_tenzin_org.data["github_app_id"]
-#   github_app_installation_id = data.vault_generic_secret.github_tenzin_org.data["github_app_installation_id"]
-#   github_app_private_key     = data.vault_generic_secret.github_tenzin_org.data["github_app_private_key"]
-# }
-# 
-# module "jupyterhub" {
-#   depends_on                   = [module.local_path_provisioner]
-#   source                       = "git::https://github.com/tenzin-io/terraform-modules.git//kubernetes/jupyterhub?ref=main"
-#   enable_github_oauth          = true
-#   github_oauth_client_id       = data.vault_generic_secret.jupyterhub.data["github_oauth_client_id"]
-#   github_oauth_client_secret   = data.vault_generic_secret.jupyterhub.data["github_oauth_client_secret"]
-#   allowed_github_organizations = ["tenzin-io"]
-#   jupyter_image_name           = "quay.io/jupyter/pytorch-notebook"
-#   jupyter_image_tag            = "pytorch-2.4.1"
-#   jupyterhub_fqdn              = "jupyterhub.tenzin.io"
-# }
-# 
-# module "grafana" {
-#   depends_on                  = [module.cert_manager, module.local_path_provisioner, module.prometheus]
-#   source                      = "git::https://github.com/tenzin-io/terraform-modules.git//kubernetes/grafana?ref=main"
-#   enable_github_oauth         = true
-#   github_oauth_client_id      = data.vault_generic_secret.grafana.data["github_oauth_client_id"]
-#   github_oauth_client_secret  = data.vault_generic_secret.grafana.data["github_oauth_client_secret"]
-#   allowed_github_organization = "tenzin-io"
-#   grafana_fqdn                = "grafana.tenzin.io"
-# }
+module "actions_runner" {
+  source     = "git::https://github.com/tenzin-io/terraform-modules.git//kubernetes/actions-runner?ref=main"
+  depends_on = [module.calico]
+}
+
+module "actions_runner_org" {
+  depends_on                 = [module.actions_runner]
+  runner_set_name            = "k8s-1-runners"
+  source                     = "git::https://github.com/tenzin-io/terraform-modules.git//kubernetes/actions-runner-set?ref=main"
+  runner_image               = "ghcr.io/tenzin-io/actions-runner:0.0.3"
+  github_config_urls         = ["https://github.com/tenzin-io"]
+  github_app_id              = data.vault_generic_secret.github_tenzin_org.data["github_app_id"]
+  github_app_installation_id = data.vault_generic_secret.github_tenzin_org.data["github_app_installation_id"]
+  github_app_private_key     = data.vault_generic_secret.github_tenzin_org.data["github_app_private_key"]
+}
+
+module "jupyterhub" {
+  depends_on                   = [module.local_path_provisioner]
+  source                       = "git::https://github.com/tenzin-io/terraform-modules.git//kubernetes/jupyterhub?ref=main"
+  enable_github_oauth          = true
+  github_oauth_client_id       = data.vault_generic_secret.jupyterhub.data["github_oauth_client_id"]
+  github_oauth_client_secret   = data.vault_generic_secret.jupyterhub.data["github_oauth_client_secret"]
+  allowed_github_organizations = ["tenzin-io"]
+  jupyter_image_name           = "quay.io/jupyter/pytorch-notebook"
+  jupyter_image_tag            = "pytorch-2.4.1"
+  jupyterhub_fqdn              = "jupyterhub.tenzin.io"
+}
+
+module "grafana" {
+  depends_on                  = [module.cert_manager, module.local_path_provisioner, module.prometheus]
+  source                      = "git::https://github.com/tenzin-io/terraform-modules.git//kubernetes/grafana?ref=main"
+  enable_github_oauth         = true
+  github_oauth_client_id      = data.vault_generic_secret.grafana.data["github_oauth_client_id"]
+  github_oauth_client_secret  = data.vault_generic_secret.grafana.data["github_oauth_client_secret"]
+  allowed_github_organization = "tenzin-io"
+  grafana_fqdn                = "grafana.tenzin.io"
+}
 
 
 # module "nvidia_gpu_operator" {
