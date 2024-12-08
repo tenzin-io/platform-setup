1d99c99cfeccc2b16cf22d493be73a3f5babbbb1
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2024-12-08 15:44:09 +0000

    Add vhost setup workspace

---
+++ 01-hypervisor-setup/vhost/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source  = "dmacvicar/libvirt"
+      version = "0.8.1"
+    }
+  }
+  backend "s3" {
+    bucket         = "tenzin-io"
+    key            = "terraform/01-hypervisor-setup/vhost.tfstate"
+    dynamodb_table = "tenzin-io"
+    region         = "us-east-1"
+  }
+}
+
+locals {
+  hypervisor_hostname     = "vhost.lan"
+  hypervisor_ssh_user     = "tenzin-bot"
+  hypervisor_ssh_key_name = "tenzin-bot.key"
+}
+
+provider "libvirt" {
+  uri = "qemu+ssh://${local.hypervisor_ssh_user}@${local.hypervisor_hostname}/system?keyfile=${local.hypervisor_ssh_key_name}&sshauth=privkey&no_verify=1"
+}
+
+resource "libvirt_pool" "cloud_images" {
+  name = "cloud-images"
+  type = "dir"
+  target {
+    path = "/data/cloud-images"
+  }
+}
+
+resource "libvirt_volume" "ubuntu_cloud_image" {
+  source = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
+  name   = "noble-server-cloudimg-amd64.img"
+  pool   = libvirt_pool.cloud_images.name
+  format = "qcow2"
+}
